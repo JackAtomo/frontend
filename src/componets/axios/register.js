4b454e211e3bab391cb8b8544f752b1bd24a2b3c
JackAtomo <54991457+JackAtomo@users.noreply.github.com>
2020-01-08 00:17:11 +0100

    Cambio Bakend

--- src/componets/axios/register.js
+++ src/componets/axios/register.js
@@ -25,7 +25,7 @@ export function register({
     postalCode,
     phone,
     bornIn);
-return axios.post(`http://localhost:8000/api/accounts`, {
+return axios.post(`https://api-rest-seguros.herokuapp.com:8000/api/accounts`, {
   firstName: firstName,
   lastName: lastName,
   email: email,
